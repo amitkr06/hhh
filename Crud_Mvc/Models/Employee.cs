02ca4752f0408bddee81c5689803dbec94845c19
amitkr06 <amit7260980@gmail.com>
2023-10-29 22:16:36 +0530

    first

---
+++ Crud_Mvc/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud_Mvc.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string city { get; set; }
+
+        public string address { get; set; }
+
+        public string dept_name { get; set; }
+
+
+    }
+}
